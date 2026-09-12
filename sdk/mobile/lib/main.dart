cfcb6304f2ac4e36dff08d543a23484e6a682c9c
fannnzhang <fannnzhang@gmail.com>
2026-09-12 21:07:39 +0800

    chore(mobile): remove sentry_flutter
    - launch no longer reads SENTRY_DSN
    - kim-sdk ownership spec drops the Sentry launch gate

--- sdk/mobile/lib/main.dart
+++ sdk/mobile/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
@@ -19,13 +18,6 @@ import 'theme/kim_theme.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await _enableMaxRefreshRate();
-  const dsn = String.fromEnvironment('SENTRY_DSN');
-  if (dsn.isNotEmpty) {
-    await SentryFlutter.init((options) {
-      options.dsn = dsn;
-    }, appRunner: () => runApp(const KimBoot()));
-    return;
-  }
   runApp(const KimBoot());
 }
 
